eefcde3c47aac9ab122c12920d3d4852df5f7b5c
koikoiti <evandrokoiti@gmail.com>
2017-02-22 23:41:30 -0300

    [FIX] ajax cliente autocomplete

--- sistema/lib/ajax/clienteautocomplete.php
+++ sistema/lib/ajax/clienteautocomplete.php
@@ -7,7 +7,7 @@
     
     $term = $_GET[ "term" ];
     
-    $Sql = 'SELECT idcliente, nome, idtipoprofissional, cnpj, cpf FROM t_clientes WHERE nome LIKE "%'.$term.'%" OR nome_contato LIKE "%'.$term.'%" OR cpf LIKE "%'.$term.'%" OR cnpj LIKE "%'.$term.'%" ORDER BY nome ASC';
+    $Sql = 'SELECT idcliente, nome, idtipoprofissional, cnpj, cpf FROM t_clientes WHERE (nome LIKE "%'.$term.'%" OR nome_contato LIKE "%'.$term.'%" OR cpf LIKE "%'.$term.'%" OR cnpj LIKE "%'.$term.'%") AND ativo = 1 ORDER BY nome ASC';
     $result = $banco->Execute($Sql);
     
     while($rs = $banco->ArrayData($result)){
