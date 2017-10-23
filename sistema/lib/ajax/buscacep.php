9ac7ac45c3ec985a21a80cf340eaf7c37d0e1edf
koikoiti <evandrokoiti@gmail.com>
2017-10-22 22:43:10 -0200

    [FIX] buscaCep

--- sistema/lib/ajax/buscacep.php
+++ sistema/lib/ajax/buscacep.php
@@ -8,46 +8,20 @@
 	#Recuperando valores
 	$cep = str_replace("-", "", $_POST["cep"]);
 	
-	$cURL = curl_init('http://m.correios.com.br/movel/buscaCepConfirma.do');
-	$dados = array(
-			'cepEntrada' => $cep,
-			'tipoCep' => '',
-			'cepTemp' => '',
-			'metodo' => 'buscarCep'
-			);
-	#url-ify the data for the POST
-	foreach($dados as $key => $value){
-		$fields_string .= $key.'='.$value.'&';
-	}
-	rtrim($fields_string, '&');
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, "http://viacep.com.br/ws/$cep/json/");
+	$result = curl_exec($ch);
+	curl_close($ch);
 	
-	curl_setopt($cURL, CURLOPT_POST, true);
-	curl_setopt($cURL, CURLOPT_POSTFIELDS, $fields_string);
-	curl_setopt($cURL, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
-	$resultado = curl_exec($cURL);
-	curl_close($cURL);
+	$obj = json_decode($result);
 	
-	$newlines = array("\t","\n","\r","\0","\x0B");
-	$resultado = str_replace($newlines, "", html_entity_decode($resultado));
-	if(strpos($resultado, 'Dados nao encontrados') !== false) {
-		echo 'NOTFOUND';die;
-	}
-	
-	$startR = strpos($resultado,'<div class="caixacampobranco">');
-	$endR = strpos($resultado,'<div style="text-align: right;" class="mopcoes orientacao">',$startR - 59);
-	$tableR = substr($resultado,$startR,$endR-$startR);
-	#Remove espaços duplicados ou +
-	$tableR = preg_replace('/\s+/', ' ',$tableR);
-	$tableR = str_replace('<span class="respostadestaque">', '', $tableR);
-		
-	$expl = explode("</span>", $tableR);
-	
-	$logradouro = trim($expl[1]);
-	$bairro = trim($expl[3]);
-	$cidade = str_replace(' /', '/', trim($expl[5]));
+	$logradouro = $obj->logradouro;
+	$bairro = $obj->bairro;
+	$cidade = $obj->localidade . "/" . $obj->uf;
 	
 	$retorno = $logradouro . "|" . $bairro . "|" . $cidade;
 	
-	echo utf8_encode($retorno);
+	echo $retorno;
 ?>
