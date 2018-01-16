8a38cab6e3ca333cbbdc0a31b3ae3d5ba8a62faf
koikoiti <evandrokoiti@gmail.com>
2018-01-16 21:47:33 -0200

    [FIX] relatorio (Vendas)

--- sistema/functions/banco-relatorio.php
+++ sistema/functions/banco-relatorio.php
@@ -1,8 +1,25 @@
 <?php
 	class bancorelatorio extends banco{
 		
+		#Select tipo pagamento
+		function MontaSelectTipoPagamento($idtipopagamento){
+			$select_pgto = "<select id='busca_pgto' style='float: left; width: 10%;' class='form-control' name='busca_pgto'>";
+			$select_pgto .= "<option selected value=''>Todos Pgtos</option>";
+			$Sql = "SELECT * FROM fixo_forma_pagamento ORDER BY forma_pagamento ASC";
+			$result = parent::Execute($Sql);
+			while($rs = parent::ArrayData($result)){
+				if($rs['idformapagamento'] == $idtipopagamento){
+					$select_pgto .= "<option selected value='".$rs['idformapagamento']."'>".$rs['forma_pagamento']."</option>";
+				}else{
+					$select_pgto .= "<option value='".$rs['idformapagamento']."'>".$rs['forma_pagamento']."</option>";
+				}
+			}
+			$select_pgto .= "</select>";
+			return utf8_encode($select_pgto);
+		}
+		
 		#Monta o relatório
-		function MontaRelatorio($dataIni, $dataFim, $idresponsavel, $marca){
+		function MontaRelatorio($dataIni, $dataFim, $idresponsavel, $marca, $idtipopagamento){
 			$Auxilio = parent::CarregaHtml('itens/relatorio-itens');
 			$where = '';
 			if($dataIni){
@@ -15,6 +32,10 @@
 			}
 			if($idresponsavel){
 				$where .= " AND V.idusuario = $idresponsavel";
+			}
+			
+			if($idtipopagamento){
+				$where .= " AND Y.idformapagamento = $idtipopagamento";
 			}
 			
 			if($marca){
@@ -37,14 +58,16 @@
 				}
 				$where = rtrim($where, " OR");
 				$where .= ")";
-				$Sql = "SELECT C.nome, C.idtipoprofissional, V.idvenda, V.data, V.valor_frete, V.valor_venda, V.idusuario
+				$Sql = "SELECT DISTINCT C.nome, C.idtipoprofissional, V.idvenda, V.data, V.valor_frete, V.valor_venda, V.idusuario
 						FROM t_vendas V
 						INNER JOIN t_clientes C ON V.idcliente = C.idcliente
 						INNER JOIN t_vendas_produtos X ON V.idvenda = X.idvenda
+						INNER JOIN t_vendas_pagamentos Y ON Y.idvenda = V.idvenda
 						WHERE 1 $where AND V.orcamento = 0 GROUP BY idvenda";
 			}else{
-				$Sql = "SELECT C.nome, V.idvenda, V.data, V.valor_frete, V.valor_venda, V.idusuario FROM t_vendas V 
-						INNER JOIN t_clientes C ON V.idcliente = C.idcliente 
+				$Sql = "SELECT DISTINCT C.nome, V.idvenda, V.data, V.valor_frete, V.valor_venda, V.idusuario FROM t_vendas V 
+						INNER JOIN t_clientes C ON V.idcliente = C.idcliente
+						INNER JOIN t_vendas_pagamentos Y ON Y.idvenda = V.idvenda
 						WHERE 1 $where AND V.orcamento = 0";
 			}
 			$quantidade_vendas = 0;
@@ -94,6 +117,7 @@
 					$Linha = str_replace("<%VALORFRETE%>", "R$ " . number_format($rs['valor_frete'], 2, ',', '.'), $Linha);
 					$Linha = str_replace("<%VALORTOTAL%>", "R$ " . number_format($valor_venda, 2, ',', '.'), $Linha);
 				}else{
+					$valor_venda = $rs['valor_venda'];
 					$venda_sem_frete = $rs['valor_venda'] - $rs['valor_frete'];
 					$Linha = str_replace("<%VENDASEMFRETE%>", "R$ " . number_format($venda_sem_frete, 2, ',', '.'), $Linha);
 					$Linha = str_replace("<%VALORFRETE%>", "R$ " . number_format($rs['valor_frete'], 2, ',', '.'), $Linha);
@@ -106,7 +130,6 @@
 				$total_vendas += $valor_venda;
 				$total_fretes += $rs['valor_frete'];
 				$total_vendas_sem_frete += $venda_sem_frete;
-				
 				$Relatorio .= $Linha;
 			}
 			$LinhaTotal = parent::CarregaHtml('itens/relatorio-ultima');
@@ -122,7 +145,7 @@
 		#Monta usuários select
 		function MontaUsuarios($idresponsavel){
 			$Sql = "SELECT * FROM t_usuarios WHERE 1 AND login <> 'admin' ORDER BY ativo DESC, nome_exibicao ASC";
-			$select_usuarios = "<select id='busca_responsavel' style='float: left; width: 25%;' class='form-control' name='busca_responsavel'>";
+			$select_usuarios = "<select id='busca_responsavel' style='float: left; width: 20%;' class='form-control' name='busca_responsavel'>";
 			$select_usuarios .= "<option selected value=''>Responsável</option>";
 			$result = parent::Execute($Sql);
 			if($result){
--- sistema/lib/relatorios.php
+++ sistema/lib/relatorios.php
@@ -14,16 +14,19 @@
     	$dataFim = $_GET['dataFim'];
     	$idresponsavel = $_GET['busca_responsavel'];
     	$marca = $_GET['busca_marca'];
+    	$idtipopagamento = $_GET['busca_pgto'];
     	$botao_limpar = '<a href="'.UrlPadrao.'relatorios" class="btn btn-danger"><i class="fa fa-times"></i></a>';
     }
     
-    $Relatorio = $banco->MontaRelatorio($dataIni, $dataFim, $idresponsavel, $marca);
+    $Relatorio = $banco->MontaRelatorio($dataIni, $dataFim, $idresponsavel, $marca, $idtipopagamento);
     $select_usuarios = $banco->MontaUsuarios($idresponsavel);
+    $select_tipoPagamento = $banco->MontaSelectTipoPagamento($idtipopagamento);
     
     #Imprime valores
 	$Conteudo = utf8_encode($banco->CarregaHtml('relatorios'));
     $Conteudo = str_replace("<%RELATORIO%>", $Relatorio, $Conteudo);
     $Conteudo = str_replace("<%SELECTUSUARIOS%>", $select_usuarios, $Conteudo);
+    $Conteudo = str_replace("<%SELECTTIPOPAGAMENTO%>", $select_tipoPagamento, $Conteudo);
     $Conteudo = str_replace("<%BUSCADATAINI%>", $dataIni, $Conteudo);
     $Conteudo = str_replace("<%BUSCADATAFIM%>", $dataFim, $Conteudo);
     $Conteudo = str_replace("<%BUSCAMARCA%>", $marca, $Conteudo);
