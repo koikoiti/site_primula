5b60bef36597a086a055d6f642f78c4025e1548e
Koiti <evandrokoiti@gmail.com>
2019-03-29 00:41:36 -0300

    [FIX] lista carteira
    add data ultima venda e cores

--- sistema/lib/ajax/listaDe.php
+++ sistema/lib/ajax/listaDe.php
@@ -1,4 +1,11 @@
 <?php
+    function diffe($date1, $date2) {
+        $start = new DateTime($date1);
+        $end = new DateTime($date2);
+        $months = round(($end->format('U') - $start->format('U')) / (60*60*24*30));
+        return $months;
+    }
+
 	include('../../functions/banco.php');
 	include('../../conf/tags.php');
 	
@@ -9,26 +16,62 @@
 	$funcDe = $_POST['funcDe'];
 	
 	if($funcDe == 'nao'){
-		$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional FROM t_usuarios_carteira_clientes X
+		$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional, MAX(V.data) AS ultima_venda FROM t_usuarios_carteira_clientes X
 				RIGHT JOIN t_clientes C ON X.idcliente = C.idcliente 
 				INNER JOIN fixo_tipo_profissional P ON C.idtipoprofissional = P.idtipoprofissional
+                LEFT JOIN t_vendas V ON V.idcliente = C.idcliente 
 				WHERE X.idcliente IS NULL
-				ORDER BY C.nome ASC";
+                GROUP BY C.idcliente
+                ORDER BY ultima_venda DESC";
 		$result = $banco->Execute($Sql);
 		while($rs = $banco->ArrayData($result)){
-			$retorno .= '<div class="col-sm-12"><label><input name="arrClientes[]" value="'.$rs['idcliente'].'" type="checkbox"> '.$rs['nome'].' ('.$rs['profissional'].')</label></div>';
+		    if($rs['ultima_venda']){
+		        $ultima_venda = date("d/m/Y", strtotime($rs['ultima_venda']));
+		        $meses_ultima_venda = diffe($rs['ultima_venda'], date("Y-m-d H:i:s"));
+		        if($meses_ultima_venda >= 0 && $meses_ultima_venda <= 3){
+		            $div_color = 'alert-success';
+		        }elseif($meses_ultima_venda > 3 && $meses_ultima_venda <= 6){
+		            $div_color = 'alert-warning';
+		        }elseif($meses_ultima_venda > 6){
+		            $div_color = 'alert-danger';
+		        }
+		    }else{
+		        $ultima_venda = "--/--/----";
+		        $meses_ultima_venda = null;
+		        $div_color = '';
+		    }
+		    		    
+			$retorno .= '<div class="col-sm-10 '.$div_color.'"><label><input name="arrClientes[]" value="'.$rs['idcliente'].'" type="checkbox"> '.$rs['nome'].' ('.$rs['profissional'].')</label></div><div class="col-sm-2 '.$div_color.'"><label style="padding-top: 2px;">'.$ultima_venda.'</label></div>';
 		}
 	}else{
-		$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional FROM t_usuarios_carteira_clientes U 
+		$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional, MAX(V.data) AS ultima_venda FROM t_usuarios_carteira_clientes U 
 				INNER JOIN t_clientes C ON U.idcliente = C.idcliente 
-				INNER JOIN fixo_tipo_profissional P ON C.idtipoprofissional = P.idtipoprofissional 
-				WHERE U.idusuario = $funcDe 
-				ORDER BY C.nome ASC";
+				INNER JOIN fixo_tipo_profissional P ON C.idtipoprofissional = P.idtipoprofissional
+                LEFT JOIN t_vendas V ON V.idcliente = C.idcliente 
+				WHERE U.idusuario = $funcDe
+                GROUP BY C.idcliente
+				ORDER BY ultima_venda DESC";
 		$result = $banco->Execute($Sql);
 		while($rs = $banco->ArrayData($result)){
-			$retorno .= '<div class="col-sm-12"><label><input name="arrClientes[]" value="'.$rs['idcliente'].'" type="checkbox"> '.$rs['nome'].' ('.$rs['profissional'].')</label></div>';
+		    if($rs['ultima_venda']){
+		        $ultima_venda = date("d/m/Y", strtotime($rs['ultima_venda']));
+		        $meses_ultima_venda = diffe($rs['ultima_venda'], date("Y-m-d H:i:s"));
+		        if($meses_ultima_venda >= 0 && $meses_ultima_venda <= 3){
+		            $div_color = 'alert-success';
+		        }elseif($meses_ultima_venda > 3 && $meses_ultima_venda <= 6){
+		            $div_color = 'alert-warning';
+		        }elseif($meses_ultima_venda > 6){
+		            $div_color = 'alert-danger';
+		        }
+		    }else{
+		        $ultima_venda = "--/--/----";
+		        $meses_ultima_venda = null;
+		        $div_color = '';
+		    }
+		    
+		    $retorno .= '<div class="col-sm-10 '.$div_color.'"><label><input name="arrClientes[]" value="'.$rs['idcliente'].'" type="checkbox"> '.$rs['nome'].' ('.$rs['profissional'].')</label></div><div class="col-sm-2 '.$div_color.'"><label style="padding-top: 2px;">'.$ultima_venda.'</label></div>';
 		}
 	}
 	
-	echo utf8_encode($retorno);	
+	echo utf8_encode($retorno);
 ?>
--- sistema/lib/ajax/listaPara.php
+++ sistema/lib/ajax/listaPara.php
@@ -1,4 +1,11 @@
 <?php
+    function diffe($date1, $date2) {
+        $start = new DateTime($date1);
+        $end = new DateTime($date2);
+        $months = round(($end->format('U') - $start->format('U')) / (60*60*24*30));
+        return $months;
+    }
+
 	include('../../functions/banco.php');
 	include('../../conf/tags.php');
 	
@@ -8,13 +15,34 @@
 	
 	$funcPara = $_POST['funcPara'];
 	
-	$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional FROM t_usuarios_carteira_clientes U
+	$Sql = "SELECT C.nome, C.idcliente, P.tipo AS profissional, MAX(V.data) AS ultima_venda FROM t_usuarios_carteira_clientes U
 			INNER JOIN t_clientes C ON U.idcliente = C.idcliente 
 			INNER JOIN fixo_tipo_profissional P ON C.idtipoprofissional = P.idtipoprofissional 
-			WHERE U.idusuario = $funcPara";
+            LEFT JOIN t_vendas V ON V.idcliente = C.idcliente
+			WHERE U.idusuario = $funcPara
+			GROUP BY C.idcliente
+			ORDER BY ultima_venda DESC";
 	$result = $banco->Execute($Sql);
 	while($rs = $banco->ArrayData($result)){
-		$retorno .= '<div class="col-sm-12"><label>'.$rs['nome'].' ('.$rs['profissional'].')</div>';
+	    if($rs['ultima_venda']){
+	        $ultima_venda = date("d/m/Y", strtotime($rs['ultima_venda']));
+	        $meses_ultima_venda = diffe($rs['ultima_venda'], date("Y-m-d H:i:s"));
+	        if($meses_ultima_venda >= 0 && $meses_ultima_venda <= 3){
+	            $div_color = 'alert-success';
+	        }elseif($meses_ultima_venda > 3 && $meses_ultima_venda <= 6){
+	            $div_color = 'alert-warning';
+	        }elseif($meses_ultima_venda > 6){
+	            $div_color = 'alert-danger';
+	        }
+	    }else{
+	        $ultima_venda = "--/--/----";
+	        $meses_ultima_venda = null;
+	        $div_color = '';
+	    }
+	    
+	   $meses_ultima_venda = diffe($rs['ultima_venda'], date("Y-m-d"));
+	    
+		$retorno .= '<div class="col-sm-10 '.$div_color.'"><label>'.$rs['nome'].' ('.$rs['profissional'].')</div><div class="col-sm-2 '.$div_color.'"><label>'.$ultima_venda.'</label></div>';
 	}
 	
 	echo utf8_encode($retorno);
